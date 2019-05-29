8550dc62b12081c21f7f646ae61beb06208f320d
Leila Weiss <leila.weiss@powerhrg.com>
2019-05-29 08:59:37 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    if params[:name].blank?
+      redirect_to controller: "sessions", action: "new"
+    else
+      session[:name] = params[:name]
+      redirect_to "/"
+    end
+  end
+
+  def destroy
+    session.delete :name if session[:name]
+    redirect_to "/"
+  end
+end
